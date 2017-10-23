5e9017bac5a54dd96d185b5be6e709d450089668
Ben Williams <ben.williams@alaska.gov>
2017-10-23 15:02:17 -0800

    initial commit

---
+++ code/scenarios/80_10/state_llp_small_vessel.R
@@ -0,0 +1,12 @@
+source('code/functions/model.R')
+
+# Define world ----
+fuel_price = 0.80 # set fuel price
+ex1 <- ex2 <- ex3 <- ex4 <- 0.10 # set ex-vessel
+
+x <- tac.state()
+x <- sim.season(x)
+x <- f.itall(x)
+
+write_csv(x, "output/80_10/state_llp_small_vessel_80_10.csv")
+
