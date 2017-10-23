d60deedabb8ca41f91d5516bb4ab962a388ac031
Ben Williams <ben.williams@alaska.gov>
2017-10-23 15:58:02 -0800

    initial commit

---
+++ code/scenarios/80_10/state_llp_equal_catch_share.R
@@ -0,0 +1,15 @@
+source('code/functions/model.R')
+
+# Define world ----
+fuel_price = 0.80 # set fuel price
+ex1 <- ex2 <- ex3 <- ex4 <- 0.10 # set ex-vessel
+
+x <- tac.state.ecs()
+x <- sim.season.p_holder(x)
+x <- f.itall(x)
+
+write_csv(x, "output/80_10/state_equal_catch_share_80_10.csv")
+
+# notes
+# no behavior change - prob of fishing remains same
+# quota is equally split across all participants/areas/ports
