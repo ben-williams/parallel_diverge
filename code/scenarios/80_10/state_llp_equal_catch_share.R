d42d8037e03e6b856649aa982c1684851601f578
Ben Williams <ben.williams@alaska.gov>
2017-10-23 16:05:23 -0800

    update ecs and add note

--- code/scenarios/80_10/state_llp_equal_catch_share.R
+++ code/scenarios/80_10/state_llp_equal_catch_share.R
@@ -6,10 +6,12 @@ ex1 <- ex2 <- ex3 <- ex4 <- 0.10 # set ex-vessel
 
 x <- tac.state.ecs()
 x <- sim.season.p_holder(x)
-x <- f.itall(x)
+x <- f.itall.ecs(x)
 
 write_csv(x, "output/80_10/state_equal_catch_share_80_10.csv")
 
 # notes
 # no behavior change - prob of fishing remains same
 # quota is equally split across all participants/areas/ports
+
+
